142a6e1e9964e4e585ae3d1ae2d2b6d00d874302
Quadeatschool <quadehafen@byui.edu>
2026-01-28 14:07:08 -0700

    Create learning.ts

---
+++ ts-file-manager/src/learning.ts
@@ -0,0 +1,23 @@
+// variables
+
+let variable: string = "hey there";
+console.log(variable);
+
+// functions
+
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+
+}
+
+// Object
+
+const quade = {
+    name: "Quade",
+    age: 24,
+    race: "weird"
+}
+
+console.log(quade.age);
+
+console.log(quade.name + quade.race)
